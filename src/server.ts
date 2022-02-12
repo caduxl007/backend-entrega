febc199bb16c2cbb851a6191a16e44d66214d011
caduxl007 <eduardo2020dev@gmail.com>
2022-02-12 12:12:42 -0300

    feature: create client

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(3000, () => {
-  console.log('Start server, port 3000')
-})
+  console.log('Start server, port 3000');
+});
