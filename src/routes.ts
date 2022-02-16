c3bb96d92d7938524acbf1145cacc9e88c40b3c1
caduxl007 <eduardo2020dev@gmail.com>
2022-02-15 23:08:31 -0300

    feat: authenticate deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { AuthenticateClientController } from './modules/account/authenticateUser/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
@@ -6,10 +7,14 @@ import { CreateDeliverymanController } from './modules/deliveryman/useCases/crea
 const routes = Router();
 
 const createClientController = new CreateClientController();
-const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 
-routes.post('/authenticate/', authenticateClientController.handle);
+const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+
+
+routes.post('/client/authenticate/', authenticateClientController.handle);
+routes.post('/deliveryman/authenticate/', authenticateDeliverymanController.handle);
 
 routes.post('/client/', createClientController.handle);
 routes.post('/deliveryman/', createDeliverymanController.handle);
